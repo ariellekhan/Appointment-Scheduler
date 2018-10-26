0f0870ac3fb819cf7ee2453ec1598b6622a1d52b
Sachin Panchoo <43559431+sachinpanchoo@users.noreply.github.com>
2018-10-25 22:48:50 -0300

    Delete BookAppointmentEventHandler.java

--- DoctorAppointmentScheduler/doctorappointmentscheduler/client/event/BookAppointmentEventHandler.java
+++
@@ -1,7 +0,0 @@
-package doctorappointmentscheduler.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BookAppointmentEventHandler extends EventHandler {
-	void onValueSubmit(BookAppointmentEvent event);
-}
