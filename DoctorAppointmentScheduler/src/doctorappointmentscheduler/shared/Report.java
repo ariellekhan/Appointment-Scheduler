69a1c0f17fcfcf8b8af8e3e195c85535c25c689d
Rahul Raychaudhuri <rahulraychaudhuri721@gmail.com>
2018-10-29 17:54:32 -0400

    Added variable names and method stubs for Report.java

--- DoctorAppointmentScheduler/src/doctorappointmentscheduler/shared/Report.java
+++ DoctorAppointmentScheduler/src/doctorappointmentscheduler/shared/Report.java
@@ -2,4 +2,22 @@ package doctorappointmentscheduler.shared;
 
 public class Report {
 
+/*
+Long id;
+name;
+appointmentTime;
+medicalCondition;
+currentMedication;
+attachedImage;
+attachedVideo;
+
+getId;
+getName;
+getTime;
+getCondition;
+getMedication;
+getHistory;
+getImage;
+getVideo;
+*/
 }
