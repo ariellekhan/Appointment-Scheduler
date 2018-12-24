ee9c651803c80d7a793a10d231534e0fea9a84c2
Sachin Panchoo <43559431+sachinpanchoo@users.noreply.github.com>
2018-12-24 13:14:25 -0400

    Delete ReportEventHandler.java

--- DoctorAppointmentScheduler/src/doctorappointmentscheduler/client/event/ReportEventHandler.java
+++
@@ -1,7 +0,0 @@
-package doctorappointmentscheduler.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ReportEventHandler extends EventHandler {
-	void onValueSubmit(ReportEvent event);
-}
